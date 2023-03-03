acfe93ac048f1b291c9b6f2ec5a8705dd562825b
anthony kugel <anthonykugel@gmail.com>
2023-03-03 07:15:29 -0600

    removed variables for mobb

--- mobb/variable.tf
+++ mobb/variable.tf
@@ -4,12 +4,6 @@ variable "resource_group_name" {
 variable "location" {
   default = "eastus"
 }
-# variable "master_subnet_id" {
-#   default = "/subscriptions/ede7f891-835c-4128-af5b-0e53848e54e7/resourceGroups/openenv-9vm4c/providers/Microsoft.Network/virtualNetworks/aro-vnet/subnets/master-subnet"
-# }
-# variable "worker_subnet_id" {
-#   default = "/subscriptions/ede7f891-835c-4128-af5b-0e53848e54e7/resourceGroups/openenv-9vm4c/providers/Microsoft.Network/virtualNetworks/aro-vnet/subnets/worker-subnet"
-# }
 variable "client_id" {
   # default = ""
 }
